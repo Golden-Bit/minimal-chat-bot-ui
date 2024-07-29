5c241d599d0aac7af5939841c319eec6dde92158
sans <simonesansalone777@gmail.com>
2024-07-29 09:43:38 +0200

    first commit to upload project

---
+++ chatbot_UI.py
@@ -0,0 +1,120 @@
+########################################################################################################################
+########################################################################################################################
+import json
+from typing import Any
+
+import requests
+import base64
+import streamlit as st
+import random
+import copy
+#from experiments.experimental_chatbots.technical_support.config import chatbot_config
+from config import chatbot_config
+
+########################################################################################################################
+########################################################################################################################
+
+api_address = chatbot_config["api_address"]
+
+########################################################################################################################
+########################################################################################################################
+
+st.set_page_config(page_title=chatbot_config["page_title"],
+                   page_icon=chatbot_config["page_icon"],
+                   layout="wide",
+                   initial_sidebar_state="auto",
+                   menu_items=None)
+
+#left_co, cent_co,last_co = st.columns(3)
+#with cent_co:
+#    st.image("https://static.wixstatic.com/media/63b1fb_4fb8962f303f4686822b59a8c284690a~mv2.png",
+#             caption=None, width=None, use_column_width=True,
+#             clamp=False, channels="RGB", output_format="auto")
+
+#st.markdown("<h1 style='text-align: center; color: white;'>Assistente Fotovoltaico </h1>", unsafe_allow_html=True)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = copy.deepcopy(chatbot_config["messages"])
+
+if "ai_avatar_url" not in st.session_state:
+
+    st.session_state.ai_avatar_url = chatbot_config["ai_avatar_url"]
+
+if "user_avatar_url" not in st.session_state:
+
+    st.session_state.user_avatar_url = chatbot_config["user_avatar_url"]
+
+########################################################################################################################
+########################################################################################################################
+
+
+def parse_streamed_output(chunk: Any):
+
+    return
+
+def main():
+
+    for message in st.session_state.messages:
+        if message["role"] == "user":
+            with st.chat_message(message["role"], avatar=st.session_state.user_avatar_url):
+                st.markdown(message["content"])
+        else:
+            with st.chat_message(message["role"], avatar=st.session_state.ai_avatar_url):
+                st.markdown(message["content"])
+
+    if prompt := st.chat_input("Say something"):
+
+        #url = f"{api_address}:8086/chat"
+
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        #####################################################################
+        # for secure
+        if len(st.session_state.messages) > 10:
+            st.session_state.messages = st.session_state.messages[-10:]
+        #####################################################################
+
+        with st.chat_message("user", avatar=st.session_state.user_avatar_url):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant", avatar=st.session_state.ai_avatar_url):
+            message_placeholder = st.empty()
+            s = requests.Session()
+            full_response = ""
+            url = "http://127.0.0.1:8109/chains/stream_chain"
+            payload = {
+                "chain_id": "qa_chain",
+                "query": {
+                    "input": prompt,
+                    "chat_history": st.session_state.messages
+                }
+            }
+
+            non_decoded_chunk = b''
+            with s.post(url, json=payload, headers=None, stream=True) as resp:
+                for chunk in resp.iter_lines():
+                    if chunk:
+
+                        if chunk.decode().startswith('''  "answer":'''):
+                            chunk = "{" + chunk.decode() + "}"
+                            chunk = json.loads(chunk)
+                            chunk = chunk["answer"]
+                            chunk = chunk.encode()
+                            print(chunk)
+                            non_decoded_chunk += chunk
+                            try:
+                                full_response += non_decoded_chunk.decode()
+                                message_placeholder.markdown(full_response + "▌",
+                                                             unsafe_allow_html=True)
+                                non_decoded_chunk = b''
+                            except UnicodeDecodeError:
+                                pass
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+########################################################################################################################
+########################################################################################################################
+
+
+main()
+#sidebar()
---
+++ config.py
@@ -0,0 +1,30 @@
+
+#######################################################
+#######################################################
+
+chatbot_config = dict()
+
+#######################################################
+
+chatbot_config["page_title"] = "Virtual Assistant For Technical Support"
+chatbot_config["page_icon"] = ("https://static.wixstatic.com/media/63b1fb_5823409f408844189cd81edc2e3720df~mv2.png")
+chatbot_config["ai_avatar_url"] = ("https://static.wixstatic.com/media/63b1fb_5823409f408844189cd81edc2e3720df~mv2.png")
+chatbot_config["user_avatar_url"] = ("https://encrypted-tbn0.gstatic.com/"
+                                        "images?q=tbn:ANd9GcQiBwY_zfAf9K1OBN1"
+                                        "eVu660SqIr6TliAKeAhAaWG5mxrLmsva8XPO"
+                                        "0xhW2kHwth4D3IWM&usqp=CAU")
+
+#chatbot_config["image_1"] = "https://static.wixstatic.com/shapes/63b1fb_3ac1a3cb810046d1aed14ff69e132b51.svg"
+chatbot_config["image_1"] = "https://static.wixstatic.com/media/63b1fb_5ead15cfc329490d83b755fe95022ba3~mv2.png"
+chatbot_config["text_1"] = "<h1 style='text-align: center; color: nightblue;'>Technical Support ChatBot</h1>"
+chatbot_config["text_2"] = "<h3 style='text-align: center; color: nightblue;'>About</h3>"
+chatbot_config["text_3"] = "Assistant specialized to answer about arguments content in provided documents."
+chatbot_config["text_4"] = "<h3 style='text-align: center; color: nightblue;'>Upload your documents</h3>"
+chatbot_config["powered_by"] = "<p style='text-align: center; color: nightblue;'>Powered by Langchain</a></p>"
+chatbot_config["copyright"] = "<h4 style='text-align: center; color: nightblue;'>© 2023 by Gold Solar srl</h4>"
+
+#######################################################
+
+#chatbot_config["api_address"] = "http://127.0.0.1"
+chatbot_config["api_address"] = "http://20.234.162.189"
+#######################################################
